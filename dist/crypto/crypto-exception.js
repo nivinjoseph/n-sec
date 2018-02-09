4d04c857206d93a5bf5b89ed4e1838baf16bd2d6
nivinjoseph <nivinjoseph@outlook.com>
2018-02-09 07:03:47 -0500

    preparing to publish new version

---
+++ dist/crypto/crypto-exception.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const n_exception_1 = require("@nivinjoseph/n-exception");
+// public
+class CryptoException extends n_exception_1.Exception {
+}
+exports.CryptoException = CryptoException;
+//# sourceMappingURL=crypto-exception.js.map
