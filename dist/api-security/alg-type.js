6daa9b50a6b3c855f4752c03b60b401d69b19ec4
nivinjoseph <nivinjoseph@outlook.com>
2018-02-25 18:22:11 -0500

    preparing to publish new version

---
+++ dist/api-security/alg-type.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// public
+var AlgType;
+(function (AlgType) {
+    AlgType[AlgType["hmac"] = 1] = "hmac";
+    AlgType[AlgType["dsig"] = 2] = "dsig";
+})(AlgType = exports.AlgType || (exports.AlgType = {}));
+//# sourceMappingURL=alg-type.js.map
