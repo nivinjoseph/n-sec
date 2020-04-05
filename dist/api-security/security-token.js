bb597905affa28d87c3f13edf163dffefc247736
nivinjoseph <nivinjoseph@outlook.com>
2020-04-05 17:32:13 -0400

    preparing to publish new version

---
+++ dist/api-security/security-token.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const n_defensive_1 = require("@nivinjoseph/n-defensive");
+class SecurityToken {
+    constructor(scheme, token) {
+        n_defensive_1.given(scheme, "scheme").ensureHasValue().ensureIsString()
+            .ensure(t => !t.contains(" "), "cannot contain space");
+        this._scheme = scheme;
+        n_defensive_1.given(token, "token").ensureHasValue().ensureIsString()
+            .ensure(t => !t.contains(" "), "cannot contain space");
+        this._token = token;
+    }
+    get scheme() { return this._scheme; }
+    get token() { return this._token; }
+    toString() {
+        return `${this._scheme} ${this._token}`;
+    }
+}
+exports.SecurityToken = SecurityToken;
+//# sourceMappingURL=security-token.js.map
